b36712710ce527f23c7713172225a545a97eaa98
UllasBC <ullasbc0@gmail.com>
2020-09-28 23:06:04 +0530

    beta version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { CurrencyWidget } from "./Components/CurrencyWidget";
+import "./App.css";
 
 function App() {
+  const [inputAmount, setInputAmount] = useState(null);
+  const [outputAmount, setOutputAmount] = useState(null);
+
+  const [inputTypeCurrency, setInputTypeCurrency] = useState("");
+  const [outputTypeCurrency, setOutputTypeCurrency] = useState("");
+
+  const [loadingMessage, setLoadingMessage] = useState("");
+
+  const isValidateInputs = () => {
+    if (inputAmount <= 0) {
+      alert("Enter a valid input amount");
+    } else if (inputTypeCurrency === "" || inputTypeCurrency === "SELECT") {
+      alert("Select a valid input currency type");
+    } else if (outputTypeCurrency === "" || outputTypeCurrency === "SELECT") {
+      alert("Select a valid output currency type");
+    } else {
+      return true;
+    }
+    return false;
+  };
+
+  const onConvertClick = () => {
+    if (isValidateInputs()) {
+      const queryParams = `amount=${inputAmount}&from=${inputTypeCurrency}&to=${outputTypeCurrency}`;
+      setLoadingMessage("Please Wait...");
+      fetch("https://api.frankfurter.app/latest?" + queryParams)
+        .then((res) => res.json())
+        .then((res) => setOutputAmount(res.rates[outputTypeCurrency]))
+        .catch((err) => console.log(err))
+        .finally(() => setLoadingMessage(""));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <CurrencyWidget
+        title={"Input Amount"}
+        onInputChange={(evt) => {
+          setInputAmount(evt.target.value);
+        }}
+        inputDisabled={false}
+        inputPlaceholder={"Enter the amount: Ex-30"}
+        inputValue={inputAmount}
+        onCurrencyTypeChange={(evt) => {
+          setInputTypeCurrency(evt.target.value);
+        }}
+      />
+      <div>
+        <button onClick={onConvertClick}>Convert</button>
+        <br />
+        <span>{loadingMessage}</span>
+      </div>
+
+      <CurrencyWidget
+        title={"Output Amount"}
+        onInputChange={() => {}}
+        inputDisabled={true}
+        inputPlaceholder={"Converted Amount"}
+        inputValue={outputAmount}
+        onCurrencyTypeChange={(evt) => {
+          setOutputTypeCurrency(evt.target.value);
+        }}
+      />
     </div>
   );
 }
---
+++ src/Components/CurrencyWidget/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { CurrencyTypes } from "../../constants";
+import PropsTypes from "prop-types";
+import './index.css';
+
+export const CurrencyWidget = (props) => {
+  return (
+    <div className='widget-wrapper'>
+      <h3>{props.title}</h3>
+      <input
+      className='widget-input'
+        type={'number'}
+        onChange={props.onInputChange}
+        placeholder={props.inputPlaceholder}
+        value={props.inputValue}
+        disabled={props.inputDisabled}
+      />
+      <select onChange={props.onCurrencyTypeChange}>
+        {CurrencyTypes.map((type, index) => (
+          <option value={type}>{type}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+CurrencyWidget.propTypes = {
+  title: PropsTypes.string.isRequired,
+  onInputChange: PropsTypes.func.isRequired,
+  inputPlaceholder: PropsTypes.string.isRequired,
+  inputValue: PropsTypes.number.isRequired,
+  onCurrencyTypeChange: PropsTypes.func.isRequired,
+  inputDisabled:PropsTypes.bool.isRequired
+};
