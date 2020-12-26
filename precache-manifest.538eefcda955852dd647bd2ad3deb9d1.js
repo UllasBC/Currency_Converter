3e1bc2df94777c2e7959bab60bcf2af8d070ab46
UllasBC <ullasbc0@gmail.com>
2020-12-26 16:41:00 +0530

    Updates

---
+++ precache-manifest.538eefcda955852dd647bd2ad3deb9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb87f8bab2dd73db7a15682478b1327",
+    "url": "/Currency_Converter/index.html"
+  },
+  {
+    "revision": "2de5bff0647ddf71bebf",
+    "url": "/Currency_Converter/static/css/main.e5d31642.chunk.css"
+  },
+  {
+    "revision": "73310958ade4d250ed82",
+    "url": "/Currency_Converter/static/js/2.b126657b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency_Converter/static/js/2.b126657b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de5bff0647ddf71bebf",
+    "url": "/Currency_Converter/static/js/main.79377120.chunk.js"
+  },
+  {
+    "revision": "320dba351f9a257e7f50",
+    "url": "/Currency_Converter/static/js/runtime-main.efbd93a2.js"
+  }
+]);
